fdfb157905616b0aabc41ddd966543b42a7e0e2e
Amir Hosein Mousavi <68806656+AmirHoseinMousavi@users.noreply.github.com>
2022-03-27 13:25:05 +0430

    Exporting final json to an excel file named Final_Dataset.xlsx.

---
+++ ExportToExcel.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+
+
+# JSON file
+f = open ('final.json', "r")
+ 
+# Reading from file
+data = json.loads(f.read())
+# Convert it to Dataframe
+df = pd.json_normalize(data)
+
+# Export Dataframe to an Excel file
+file_name = 'Final_Dataset.xlsx'
+df.to_excel(file_name,index=False)
+
