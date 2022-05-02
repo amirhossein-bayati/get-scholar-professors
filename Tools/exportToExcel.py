3bde9c0605ed4a90d39ff1892a8bf820ab17fdd1
MohammadKhalaj <mohammadkhalaj@outlook.com>
2022-05-02 19:44:35 +0430

    add bayati's dataset and merge all datests to one.

--- Tools/exportToExcel.py
+++ Tools/exportToExcel.py
@@ -1,18 +1,19 @@
 import json
 
 import pandas as pd
+import sys
 
 """This module reads all data that 
 scraped from google scholar ('Json/final.json' file)
 and convert them to the excel file.
 """
 
-with open("Json/final.json", "r") as f:
+with open(sys.argv[1], "r") as f:
     # Reading from file
     data = json.loads(f.read())
     # Convert it to Dataframe
     df = pd.json_normalize(data)
 
 # Export Dataframe to an Excel file
-file_name = "Json/Final_Dataset.xlsx"
+file_name = "Final_Dataset.xlsx"
 df.to_excel(file_name, index=False)
